64e4dc754a28c5fa864a860929e80296dfaeb34c
Hatsuki Maruyama <hmr@wassha.com>
2018-10-30 22:24:59 +0900

    add dot-bashrc_env

--- install.sh
+++ install.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
+function add_into_bashrc () {
+	if [ -z "$1" ]; then
+		echo "add_into_bashrc(): Not enough args"
+		return
+	fi
+	grep "$1" ~/.bashrc >& /dev/null
+	if [ $? -ne 0 ]; then
+		echo "    Installing $1"
+		echo "source ~/.$1   #$1" >> ~/.bashrc
+	else
+		echo "    $1 is already installed."
+	fi
+}
 
 SRCDIR=`pwd`
 
-SRCS=".inputrc .vim .vimrc .bashrc_history .bashrc_alias .byobu .bashrc_ssh-agent"
+SRCS=".inputrc .vim .vimrc .bashrc_history .bashrc_alias .byobu .bashrc_ssh-agent .bashrc_env"
 DATE=`date +'%Y%M%d_%H%M%S'`
 
 
@@ -11,7 +24,6 @@ cd ~
 for TARGET in ${SRCS}
 do
     TARGETSRC=`echo ${TARGET} | sed -e 's/^\./dot-/'`
-	echo
 	echo Installing ${TARGET} from ${TARGETSRC}
 	cd ~
 	if [ -e ${TARGET} ]; then
@@ -28,32 +40,14 @@ do
 	# Make symlink
 	echo "    Making symbolic link"
 	ln -sf ${SRCDIR}/${TARGETSRC} ${TARGET}
+	echo
 done
 
 echo Adding to bashrc
-grep .bashrc_history .bashrc >& /dev/null
-if [ $? -ne 0 ]; then
-	echo "    Installing bashrc_history"
-	echo "source ~/.bashrc_history   #bashrc_history" >> .bashrc
-else
-	echo "    bashrc_history is already installed."
-fi
-
-grep .bashrc_alias .bashrc >& /dev/null
-if [ $? -ne 0 ]; then
-	echo "    Installing bashrc_alias"
-	echo "source ~/.bashrc_alias   #bashrc_alias" >> .bashrc
-else
-	echo "    bashrc_alias is already installed."
-fi
-
-grep .bashrc_ssh-agent .bashrc >& /dev/null
-if [ $? -ne 0 ]; then
-	echo "    Installing bashrc_ssh-agent"
-	echo "source ~/.bashrc_ssh-agent   #bashrc_ssh-agent" >> .bashrc
-else
-	echo "    bashrc_ssh-agent is already installed."
-fi
+add_into_bashrc bashrc_history
+add_into_bashrc bashrc_alias
+add_into_bashrc bashrc_env
+add_into_bashrc bashrc_ssh-agent
 
 grep '#byobu-prompt#' .bashrc >& /dev/null
 if [ $? -ne 0 ]; then
