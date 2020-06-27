6162b68df21d6d465038b90e5aab0b9d83cc0b5a
hmr@entropy3 <hmr@entropy3.local>
2020-06-28 00:02:40 +0900

    retabbed.

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # GPProfile install script
 # #ORIGIN: 2018/10/11 by hmr
-# vim: ft=bash
+# vim: ft=bash:ts=2:sw=0:et
 
 function get_git_hash() {
   local gitHash=$(git rev-parse --short HEAD)
@@ -29,66 +29,66 @@ abs_dirname() {
 }
 
 function del_from_bashrc () {
-	if [ -z "$1" ]; then
-		echo "del_from_bashrc(): Not enough args"
-		return
-	fi
-	grep "$1" ~/.bashrc >& /dev/null
-	if [ $? -eq 0 ]; then
-		echo "    Uninstalling $1"
-		sed -i -e "/$1/d" ~/.bashrc
-	else
-		echo "    $1 is already uninstalled."
-	fi
+  if [ -z "$1" ]; then
+    echo "del_from_bashrc(): Not enough args"
+    return
+  fi
+  grep "$1" ~/.bashrc >& /dev/null
+  if [ $? -eq 0 ]; then
+    echo "    Uninstalling $1"
+    sed -i -e "/$1/d" ~/.bashrc
+  else
+    echo "    $1 is already uninstalled."
+  fi
 }
 
 function add_into_bashrc () {
-	if [ -z "$1" ]; then
-		echo "add_into_bashrc(): Not enough args"
-		return
-	fi
-	grep "$1" ~/.bashrc >& /dev/null
-	if [ $? -ne 0 ]; then
-		echo "    Installing $1"
-		echo "source ~/.$1   #$1[gpprofile]" >> ~/.bashrc
-	else
-		echo "    $1 is already installed."
-	fi
+  if [ -z "$1" ]; then
+    echo "add_into_bashrc(): Not enough args"
+    return
+  fi
+  grep "$1" ~/.bashrc >& /dev/null
+  if [ $? -ne 0 ]; then
+    echo "    Installing $1"
+    echo "source ~/.$1   #$1[gpprofile]" >> ~/.bashrc
+  else
+    echo "    $1 is already installed."
+  fi
 }
 
 function add_call_bashrc_into_bash_profile () {
-	grep ".bashrc" ~/.bash_profile >& /dev/null
-	if [ $? -ne 0 ]; then
-		echo "    Adding bashrc call routine into bash_profile."
-		echo "[ -r ~/.bashrc ] && . ~/.bashrc #call bashrc[gpprofile]" >> ~/.bash_profile
-	else
-		echo "    bashrc call routine is already installed."
-	fi
+  grep ".bashrc" ~/.bash_profile >& /dev/null
+  if [ $? -ne 0 ]; then
+    echo "    Adding bashrc call routine into bash_profile."
+    echo "[ -r ~/.bashrc ] && . ~/.bashrc #call bashrc[gpprofile]" >> ~/.bash_profile
+  else
+    echo "    bashrc call routine is already installed."
+  fi
 }
 
 function add_into_bash_profile () {
-	if [ -z "$1" ]; then
-		echo "add_into_bash_profile(): Not enough args"
-		return
-	fi
-	grep "$1" ~/.bash_profile >& /dev/null
-	if [ $? -ne 0 ]; then
-		echo "    Installing $1"
-		echo "source ~/.$1   #$1[gpprofile]" >> ~/.bash_profile
-	else
-		echo "    $1 is already installed."
-	fi
+  if [ -z "$1" ]; then
+    echo "add_into_bash_profile(): Not enough args"
+    return
+  fi
+  grep "$1" ~/.bash_profile >& /dev/null
+  if [ $? -ne 0 ]; then
+    echo "    Installing $1"
+    echo "source ~/.$1   #$1[gpprofile]" >> ~/.bash_profile
+  else
+    echo "    $1 is already installed."
+  fi
 }
 
 function add_into_bashrc_before_specified_line () {
-	if [ -z "$2" ]; then
-		echo "add_into_bashrc_before_specified_line(): Not enough arg(s)"
-		return
-	fi
-	grep "$2" ~/.bashrc >& /dev/null && echo "    GPProfile signboard is alreasy installed." && return
+  if [ -z "$2" ]; then
+    echo "add_into_bashrc_before_specified_line(): Not enough arg(s)"
+    return
+  fi
+  grep "$2" ~/.bashrc >& /dev/null && echo "    GPProfile signboard is alreasy installed." && return
 
-	echo "    Adding GPProfiles signboard."
-	sed -i ~/.bashrc -e "/$1/i $2"
+  echo "    Adding GPProfiles signboard."
+  sed -i ~/.bashrc -e "/$1/i $2"
 }
 
 
@@ -118,14 +118,14 @@ cd ~
 for TARGET in ${DEL_SRCS}
 do
     TARGETSRC=`echo ${TARGET} | sed -e 's/^\./dot-/'`
-	echo Deleting ${TARGET}
-	cd ~
-	if [ -L ${TARGET} ]; then
-		echo "    Deleting symbolic link ${TARGET}"
-		rm ${TARGET}
-	else
-		echo "    ${TARGET} doesn't exist."
-	fi
+  echo Deleting ${TARGET}
+  cd ~
+  if [ -L ${TARGET} ]; then
+    echo "    Deleting symbolic link ${TARGET}"
+    rm ${TARGET}
+  else
+    echo "    ${TARGET} doesn't exist."
+  fi
 done
 echo
 
@@ -133,23 +133,23 @@ echo
 for TARGET in ${SRCS}
 do
     TARGETSRC=`echo ${TARGET} | sed -e 's/^\./dot-/'`
-	echo Installing ${TARGET} from ${TARGETSRC}
-	cd ~
-	if [ -e ${TARGET} ]; then
-		if [ -L ${TARGET} ]; then
-			# Delete Symlink
-			echo "    Deleting symbolic link ${TARGET}"
-			rm -f ${TARGET}
-		else
-			# Move to backup
-			echo "    Backing up ${TARGET} as ${TARGET}.${DATE}"
-			mv ${TARGET} ${TARGET}.${DATE}
-		fi
-	fi
-	# Make symlink
-	echo "    Making symbolic link"
-	ln -sf ${SRCDIR}/${TARGETSRC} ${TARGET}
-	echo
+  echo Installing ${TARGET} from ${TARGETSRC}
+  cd ~
+  if [ -e ${TARGET} ]; then
+    if [ -L ${TARGET} ]; then
+      # Delete Symlink
+      echo "    Deleting symbolic link ${TARGET}"
+      rm -f ${TARGET}
+    else
+      # Move to backup
+      echo "    Backing up ${TARGET} as ${TARGET}.${DATE}"
+      mv ${TARGET} ${TARGET}.${DATE}
+    fi
+  fi
+  # Make symlink
+  echo "    Making symbolic link"
+  ln -sf ${SRCDIR}/${TARGETSRC} ${TARGET}
+  echo
 done
 
 #####
@@ -162,10 +162,10 @@ add_into_bashrc bashrc_etc
 
 grep '#byobu-prompt#' .bashrc >& /dev/null
 if [ $? -ne 0 ]; then
-	echo "    Installing byobu-prompt"
-	echo "[ -r ~/.byobu/prompt ] && . ~/.byobu/prompt   #byobu-prompt[gpprofile]#" >> .bashrc
+  echo "    Installing byobu-prompt"
+  echo "[ -r ~/.byobu/prompt ] && . ~/.byobu/prompt   #byobu-prompt[gpprofile]#" >> .bashrc
 else
-	echo "    byobu-prompt is already installed."
+  echo "    byobu-prompt is already installed."
 fi
 echo
 
@@ -179,10 +179,10 @@ echo Adding to bash_profile
 # bashrc callup routine.
 grep ".bashrc" ~/.bash_profile >& /dev/null
 if [ $? -ne 0 ]; then
-	echo "    Installing bashrc call routine."
-	echo "[ -r ~/.bashrc ] && . ~/.bashrc #call bashrc[gpprofile]" >> ~/.bash_profile
+  echo "    Installing bashrc call routine."
+  echo "[ -r ~/.bashrc ] && . ~/.bashrc #call bashrc[gpprofile]" >> ~/.bash_profile
 else
-	echo "    bashrc call routine is already installed."
+  echo "    bashrc call routine is already installed."
 fi
 add_call_bashrc_into_bash_profile
 add_into_bash_profile bash_profile_ssh-agent
