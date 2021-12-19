ff05b4653630363cfd18eca83414e7f6f8149dbf
Ubuntu <ubuntu@mh-rp4-3>
2021-12-19 15:02:41 +0900

    [shell] fix around XDG

--- settings/apps/shell_common/xdg/make_xdg_base_directories.sh
+++ settings/apps/shell_common/xdg/make_xdg_base_directories.sh
@@ -14,7 +14,7 @@ if [ -d "${XDG_CONFIG_HOME}" ]; then
   echo "XDG_CONFIG_HOME exists. skip"
 else
   echo "Making XDG_CONFIG_HOME[${XDG_CONFIG_HOME}]"
-  mkdir "${XDG_CONFIG_HOME}"
+  mkdir -p "${XDG_CONFIG_HOME}"
 fi
 
 # XDG_CACHE_HOME
@@ -26,7 +26,7 @@ if [ -d "${XDG_CACHE_HOME}" ]; then
   echo "XDG_CACHE_HOME exists. skip"
 else
   echo "Making XDG_CACHE_HOME[${XDG_CACHE_HOME}]"
-  mkdir "${XDG_CACHE_HOME}"
+  mkdir -p "${XDG_CACHE_HOME}"
 fi
 
 # XDG_DATA_HOME
@@ -38,7 +38,7 @@ if [ -d "${XDG_DATA_HOME}" ]; then
   echo "XDG_DATA_HOME exists. skip"
 else
   echo "Making XDG_DATA_HOME[${XDG_DATA_HOME}]"
-  mkdir "${XDG_DATA_HOME}"
+  mkdir -p "${XDG_DATA_HOME}"
 fi
 
 # XDG_STATE_HOME
@@ -50,7 +50,7 @@ if [ -d "${XDG_STATE_HOME}" ]; then
   echo "XDG_STATE_HOME exists. skip"
 else
   echo "Making XDG_STATE_HOME[${XDG_STATE_HOME}]"
-  mkdir "${XDG_STATE_HOME}"
+  mkdir -p "${XDG_STATE_HOME}"
 fi
 
 # XDG_RUNTIME_DIR
@@ -62,7 +62,7 @@ if [ -d "${XDG_RUNTIME_DIR}" ]; then
   echo "XDG_RUNTIME_DIR exists. skip"
 else
   echo "Making XDG_RUNTIME_DIR[${XDG_RUNTIME_DIR}]"
-  mkdir "${XDG_RUNTIME_DIR}"
+  mkdir -p "${XDG_RUNTIME_DIR}"
   chmod 700 "${XDG_RUNTIME_DIR}"
 fi
 
