1df5b8dd5cc28612ed21b9eff6ca1b5ef008e9f6
hmr@entropy4 <hmr@entropy4.local>
2024-02-05 04:33:53 +0900

    [installer] devide common part

---
+++ libexec/install/install_settings_apps_common.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# vim: set ft=sh syn=bash ts=4 sw=4 :
+
+if [[ -z $TARGET ]]; then
+    echo "Error: Env TARGET isn't set."
+    exit 1
+fi
+
+
+export GPP_HOME="${GPP_HOME:=${HOME:?}/src/gpprofiles}"
+export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:=${HOME:?}/.config}"
+
+echo "TARGET: $TARGET"
+echo "GPP_HOME: $GPP_HOME"
+echo "XDG_CONFIG_HOME: $XDG_CONFIG_HOME"
+echo
+
+for APP in "${TARGET[@]:?}"
+do
+    echo "Installing ${APP:?}"
+    #TODO: make a better logic...
+    if [[ -d ${XDG_CONFIG_HOME:?}/${APP} ]]; then
+        echo "  Moving old config as ${APP}.orig"
+	mv "${XDG_CONFIG_HOME}/${APP}" "${XDG_CONFIG_HOME}/${APP}.orig"
+    fi
+
+    # Some apps need special treatment
+    if [[ ${APP} = "vim" ]]; then
+	ln -s "${GPP_HOME}/settings/apps/vim/dot-vim" "${XDG_CONFIG_HOME}/vim"
+    else
+	ln -s "${GPP_HOME}/settings/apps/${APP}" "${XDG_CONFIG_HOME}"
+    fi
+done
+
--- libexec/install/install_settings_apps_ubuntu.sh
+++ libexec/install/install_settings_apps_ubuntu.sh
@@ -3,23 +3,5 @@
 
 TARGET=(alsa ansible bash bat byobu cspell dircolors git htop less lsd  mozilla quilt readline ripgrep tmux vim zsh)
 
-export GPP_HOME="${GPP_HOME:=${HOME:?}/src/gpprofiles}"
-export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:=${HOME:?}/.config}"
-
-for APP in "${TARGET[@]:?}"
-do
-    echo "Installing ${APP:?}"
-    #TODO: make a better logic...
-    if [[ -d ${XDG_CONFIG_HOME:?}/${APP} ]]; then
-        echo "  Moving old config as ${APP}.orig"
-	mv "${XDG_CONFIG_HOME}/${APP}" "${XDG_CONFIG_HOME}/${APP}.orig"
-    fi
-
-    # Some apps need special treatment
-    if [[ ${APP} = "vim" ]]; then
-	ln -s "${GPP_HOME}/settings/apps/vim/dot-vim" "${XDG_CONFIG_HOME}/vim"
-    else
-	ln -s "${GPP_HOME}/settings/apps/${APP}" "${XDG_CONFIG_HOME}"
-    fi
-done
+. ./install_settings_apps_common.sh
 
