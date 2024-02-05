ca2c5a822a18aaf27604f3b216534446a49a5d21
hmr@entropy4 <hmr@entropy4.local>
2024-02-06 03:30:19 +0900

    [installer] small fix

--- libexec/install/install_settings_apps_ubuntu.sh
+++ libexec/install/install_settings_apps_ubuntu.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # vim: set ft=sh syn=bash ts=4 sw=4 :
 
-TARGET=(alsa ansible bash bat byobu cspell dircolors git htop less lsd  mozilla quilt readline ripgrep tmux vim zsh)
+TARGET=(alsa ansible bash bat byobu cspell dircolors git gpprofiles htop less lsd  mozilla quilt readline ripgrep tmux vim zsh)
 
 SDIR="$(cd $(dirname $0); pwd)"
 . ${SDIR}/install_settings_apps_common.sh
