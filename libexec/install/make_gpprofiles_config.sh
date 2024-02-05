c5f949a2fd116cbfb728d4fdaf5fb410dbdfbe99
hmr@entropy4 <hmr@entropy4.local>
2024-02-06 03:33:13 +0900

    [installer] small fix

--- libexec/install/make_gpprofiles_config.sh
+++ libexec/install/make_gpprofiles_config.sh
@@ -2,9 +2,12 @@
 # vim: set ft=sh syn=bash ts=4 sw=4 :
 
 SDIR="$(cd $(dirname $0)/../..; pwd)"
+pushd "${SDIR}"
 
 sed 	-e "s/__GPP_VERSION__/$(git log -1 --pretty=%H | cut -c 1-7)/" \
        	-e "s|__GPP_HOME__|${SDIR}|" \
         ${HOME:?}/.config/gpprofiles/gpprofiles.tmpl \
 	| tee ${HOME:?}/.config/gpprofiles/gpprofiles
+
+popd
 
