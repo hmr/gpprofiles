4e33867b2ee1fe173f7f7dc7baa41d074dfcce86
hmr@entropy4 <hmr@entropy4.local>
2024-04-30 23:06:01 +0900

    [install] add more install tools
    - launchd directory
    - plist templates for jedid, VIMINIT, ZDOTDIR
    - plist generation scripts
    - jedid.bash is ranamed to jedid

---
+++ libexec/install/make_com.github.hmr.gpp.env.viminit.plist.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# vim: set ft=sh syn=bash ts=4 sw=4 :
+
+TARGET="${HOME:?}/.config/launchd/com.github.hmr.gpp.env.viminit"
+
+SDIR="$(cd $(dirname $0)/../.. || { echo "Can't find SDIR."; exit; }; pwd)"
+pushd "${SDIR}" || { echo "Can't cd to SDIR."; exit; }
+
+sed 	-e "s/__GPP_VERSION__/$(git log -1 --pretty=%H | cut -c 1-7)/" \
+       	-e "s|__GPP_HOME__|${SDIR}|" \
+        -e "s|__XDG_CONFIG_HOME__|${XDG_CONFIG_HOME:=${HOME:?}/.config}|" \
+        "${TARGET}.plist.tmpl" \
+	| tee "${TARGET}.plist"
+
+popd
+
