d052575ec027d084c3d576cd3dab6cd5cb5d7c60
Hatchi Maru <890776+hmr@users.noreply.github.com>
2026-06-10 00:37:38 +0900

    [term] add iTerm2 and Sixel sample

--- libexec/tools/term/comprehensive/showcase_3.sh
+++ libexec/tools/term/comprehensive/showcase_3.sh
@@ -73,16 +73,11 @@ echo "שרה"
 echo
 
 echo "# sixel graphics"
-printf '\eP0;0;0q"1;1;64;64#0;2;0;0;0#1;2;100;100;100#1~{wo_!11?@FN^!34~^NB
-@?_ow{~$#0?BFN^!11~}wo_!34?_o{}~^NFB-#1!5~}{o_!12?BF^!25~^NB@??ow{!6~$#0!5?
-@BN^!12~{w_!25?_o{}~~NFB-#1!10~}w_!12?@BN^!15~^NFB@?_w{}!10~$#0!10?@F^!12~}
-{o_!15?_ow{}~^FB@-#1!14~}{o_!11?@BF^!7~^FB??_ow}!15~$#0!14?@BN^!11~}{w_!7?_
-w{~~^NF@-#1!18~}{wo!11?_r^FB@??ow}!20~$#0!18?@BFN!11~^K_w{}~~NF@-#1!23~M!4?
-_oWMF@!6?BN^!21~$#0!23?p!4~^Nfpw}!6~{o_-#1!18~^NB@?_ow{}~wo!12?@BFN!17~$#0!
-18?_o{}~^NFB@?FN!12~}{wo-#1!13~^NB@??_w{}!9~}{w_!12?BFN^!12~$#0!13?_o{}~~^F
-B@!9?@BF^!12~{wo_-#1!8~^NFB@?_w{}!19~{wo_!11?@BN^!8~$#0!8?_ow{}~^FB@!19?BFN
-^!11~}{o_-#1!4~^NB@?_ow{!28~}{o_!12?BF^!4~$#0!4?_o{}~^NFB!28?@BN^!12~{w_-#1
-NB@???GM!38NMG!13?@BN$#0?KMNNNF@!38?@F!13NMK-\e\'
+zcat sixel-200x100.gz
+echo
+
+echo "# iTerm Image Protocol"
+zcat iterm-200x100.gz
 echo
 
 echo "# Kitty Graphics Protocol"
