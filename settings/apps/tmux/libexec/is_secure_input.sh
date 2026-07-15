5434b3ffe3f27859a4731fda49982e852c5bd5fe
Hatchi Maru <890776+hmr@users.noreply.github.com>
2026-07-15 16:15:21 +0900

    [tmux] add macOS Secure Input indicator

---
+++ settings/apps/tmux/libexec/is_secure_input.sh
@@ -0,0 +1,6 @@
+#!/bin/dash
+
+ioreg -l -d 1 -w 0 | grep -q "SecureInput" \
+  && echo -n " !SECURE INPUT ENABLED! " # || echo -n "#[bg=green,fg=colour0] secure input not enabled "
+
+# vim: set ft=sh syn=sh ff=unix fenc=utf-8 ts=2 sw=2 sts=2 et:
