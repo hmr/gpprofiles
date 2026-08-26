1bf87ed6c3995b29fd4d6941169982e86fa9d376
Hatchi Maru <890776+hmr@users.noreply.github.com>
2026-08-26 14:37:20 +0900

    [tmux] Fix SI determination logic

--- settings/apps/tmux/libexec/is_secure_input.sh
+++ settings/apps/tmux/libexec/is_secure_input.sh
@@ -1,6 +1,6 @@
 #!/bin/dash
 
-ioreg -l -d 1 -w 0 | grep -q "SecureInput" \
-  && echo -n " SI " # || echo -n "#[bg=green,fg=colour0] secure input not enabled "
+PID="$(ioreg -l -d 1 -w 0 | grep -Po '"[a-zA-Z]+SecureInputPID"=[0-9]+' | uniq | cut -d= -f2)"
+ps -p "$PID" > /dev/null 2>&1 /dev/null && echo -n " SI "
 
 # vim: set ft=sh syn=sh ff=unix fenc=utf-8 ts=2 sw=2 sts=2 et:
